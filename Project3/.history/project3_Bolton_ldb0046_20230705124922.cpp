ac0939026acccc25a29242e8255cc877715441ca
Logan Bolton <loganbolton101@gmail.com>
2023-07-05 12:49:50 -0500

    init

---
+++ Project3/.history/project3_Bolton_ldb0046_20230705124922.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <cstdlib> //for exit()
+using namespace std;
+int main()
+{
+    ifstream inStream;
+    int data;
+    cout << "file name:";
+    // cin >> filename;
+    // cout << "entered filename is:" << filename << endl;
+    // Pass the file name as an array of chars to open()
+    // inStream.open(filename);
+    inStream.open((char*)filename.c_str());
+    if (inStream.fail()) {
+        cout << "Input file opening failed." << endl;
+        exit(1);
+    }
+    inStream >> data;
+    while (!inStream.eof()) {
+        cout << data << endl;
+        inStream >> data;
+    }
+    inStream.close();
+    return 0;
+}
