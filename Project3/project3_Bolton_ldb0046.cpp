3affa81f070cc7ad58fe2ed72c0ee61cfc4cd3c2
Logan Bolton <loganbolton101@gmail.com>
2023-07-05 19:15:17 -0500

    sort

--- Project3/project3_Bolton_ldb0046.cpp
+++ Project3/project3_Bolton_ldb0046.cpp
@@ -1,8 +1,11 @@
+//I used ChatGPT to learn how to use the bubble sort algorithm 
+
 #include <fstream>
 #include <iostream>
 #include <cstdlib> //for exit()
 #include <string>
 using namespace std;
+
 
 const int MAX_SIZE = 100;
 
@@ -21,16 +24,32 @@ int readfile(int inputArray[], ifstream& inStream){
 int sort(int array1[], int array1_size, int array2[], int array2_size, int output[]) {
     int combinedSize = array1_size + array2_size;
     int combinedArray[combinedSize];
-    for (int i = 0; i < array1_size; i++) {
-        output[i] = array1[i];
+
+    //Create a combined array of array1 and array2
+    for (int i = 0; i < array1_size; i++) { 
+        combinedArray[i] = array1[i];
+    }
+    for (int i = 0; i < array2_size; i++) {
+        combinedArray[i+array1_size] = array2[i];
     }
 
-    for (int i = 0; i < array2_size; i++) {
-        output[i+array1_size] = array2[i];
+    //Bubble Sort algorithm
+    for(int i = 0; i < combinedSize-1; i++) {      
+        for (int j = 0; j < combinedSize-i-1; j++) { 
+            if (combinedArray[j] > combinedArray[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = combinedArray[j];
+                combinedArray[j] = combinedArray[j+1];
+                combinedArray[j+1] = temp;
+            }
+        }
     }
+
+    cout << "The sorted list of " << combinedSize
+        << " numbers is: ";
 
     for (int i = 0; i < combinedSize; i++) {
-        cout << output[i] << endl;
+        cout << combinedArray[i] << " " ;
     }
 
     return combinedSize;
