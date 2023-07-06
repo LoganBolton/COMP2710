470dc64784d09e033451777938fb3f6b19071772
Logan Bolton <loganbolton101@gmail.com>
2023-07-05 21:12:30 -0500

    comments

--- Project3/project3_Bolton_ldb0046.cpp
+++ Project3/project3_Bolton_ldb0046.cpp
@@ -1,25 +1,51 @@
-//I used ChatGPT to learn how to use the bubble sort algorithm 
+//Logan Bolton - ldb0046
+//project1_Bolton_ldb0046
+//I used ChatGPT to learn the format of the bubble sort algorithm 
+//Compile : g++ project3_Bolton_ldb0046.cpp
+//Run : ./a.out
+
+//Questions 
+// Can I use the bubble sort algorithm?
+// Are input comments correct?
+// Are the variables in the main function conisidered global variables?
 
 #include <fstream>
 #include <iostream>
 #include <cstdlib> //for exit()
-#include <string>
 using namespace std;
 
 const int MAX_SIZE = 100;
 
-int readfile(int inputArray[], ifstream& inStream){ 
+//Prints out the integers in the inputted file name.
+//Input: 1) The array whose length is going to be determined.
+//       2) input file stream object
+//       3) the name of file that the array is from
+//Output: returns the length of the inputted array
+int readfile(int inputArray[], ifstream& inStream, string fileName){ 
     int index = 0;
     inStream >> inputArray[index];
     while (!inStream.eof()) {
-        // cout << inputArray[index] << endl;
         index++;
-        inStream >> inputArray[index];
+        inStream >> inputArray[index]; // Writes the current line to the array
     }
     inStream.close();
+
+    cout << "The list of " << index
+        << " numbers is:" << endl;
+
+    for (int i = 0; i < index; i++) { // Prints the array from the file
+        cout << inputArray[i] << endl;
+    }
+    cout << endl;
+
     return index;
 }
 
+// Input: 1) the first array that is to be sorted
+//        2) the size of the first array
+//        3) the second array that is to be sorted 
+//        4) the size of the second array
+// Output: Returns the pointer array of the sorted integer array
 int* sort(int array1[], int array1_size, int array2[], int array2_size) {
     int combinedSize = array1_size + array2_size;
     int* combinedArray = new int[combinedSize];
@@ -36,7 +62,7 @@ int* sort(int array1[], int array1_size, int array2[], int array2_size) {
     for(int i = 0; i < combinedSize-1; i++) {      
         for (int j = 0; j < combinedSize-i-1; j++) { 
             if (combinedArray[j] > combinedArray[j+1]) {
-                // Swap arr[j] and arr[j+1]
+                // Swap combinedArray[j] and combinedArray[j+1]
                 int temp = combinedArray[j];
                 combinedArray[j] = combinedArray[j+1];
                 combinedArray[j+1] = temp;
@@ -44,16 +70,20 @@ int* sort(int array1[], int array1_size, int array2[], int array2_size) {
         }
     }
 
-    cout << "The sorted list of " << combinedSize
-        << " numbers is: ";
+    cout << "The sorted list of " << combinedSize << " numbers is: ";
 
     for (int i = 0; i < combinedSize; i++) {
-        cout << combinedArray[i] << " " ;
+        cout << combinedArray[i] << " ";
     }
     cout << endl;
+
     return combinedArray;
 }
 
+// Creates a file that contains the provided sorted array
+// Input: 1) the pointer array of the sorted integer array
+//        2) the name of the new file
+//        3) The size of the sorted array
 void writeFile(int* sortedArray, string fileName, int sortedSize) {
 
     ofstream file(fileName); 
@@ -70,42 +100,57 @@ void writeFile(int* sortedArray, string fileName, int sortedSize) {
 }
 
 int main() {
-
+    //Initialized variables
     int array1[MAX_SIZE];
     int array1_size;
     int array2[MAX_SIZE];
     int array2_size;
     int output[MAX_SIZE];
     
+    string fileName1;
+    string fileName2;
+    string fileName3;
     ifstream inStream;
-    string fileName1 = "input1.txt";
-    string fileName2 = "input2.txt";
     
+    // Begin Program
+    cout << "*** Welcome to Logan's sorting program ***" << endl;
+    cout << "Enter the first input file name: ";
+    cin >> fileName1;
+
     // Input file 1
     inStream.open(fileName1);
     if (inStream.fail()) {
         cout << "Input file opening failed." << endl;
         exit(1);
     }
-    array1_size = readfile(array1, inStream);
+    array1_size = readfile(array1, inStream, fileName1); //prints out the file
     inStream.close();
 
-    // input File 2
+    // Input File 2
+    cout << "Enter the second input file name: ";
+    cin >> fileName2;
+
     inStream.open(fileName2);
     if (inStream.fail()) {
         cout << "Input file opening failed." << endl;
         exit(1);
     }
-    array2_size = readfile(array2, inStream);
+
+    array2_size = readfile(array2, inStream, fileName2); //prints out the file
     inStream.close();
 
-    //Creates and prints out a sorted array of the two arrays
+    // Creates and prints out a sorted array of the two arrays
     int* sortedArray = sort(array1, array1_size, array2, array2_size);
 
-    int combinedSize = array1_size + array2_size;
-    string fileName3 = "output.txt";
-    writeFile(sortedArray, fileName3, combinedSize);
+    cout << "Enter the output file name: ";
+    cin >> fileName3;
 
-    delete[] sortedArray; // remove managed memory
+    int combinedSize = array1_size + array2_size;
+    writeFile(sortedArray, fileName3, combinedSize); // Creates a file from the sorted array
+
+    cout << "*** Please check the new file - " << fileName3 << " ***" << endl;
+    cout << "*** Goodbye. ***" << endl;
+
+    delete[] sortedArray; // removes managed memory
     return 0;
 }
