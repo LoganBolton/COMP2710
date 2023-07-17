b7ad1492290bac3a53abcab9b1fbc013a60e7009
Logan Bolton <loganbolton101@gmail.com>
2023-07-17 13:13:19 -0500

    linked list

---
+++ Project4/project4_Bolton_ldb0046.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct TriviaNode {
+    string question;
+    string answer;
+    int points;
+    TriviaNode* next;
+
+    TriviaNode(
+        string questionInput, 
+            string answerInput, 
+            int pointsInput, 
+            TriviaNode* nextInput = nullptr)
+
+        : question(questionInput), 
+            answer(answerInput), 
+            points(pointsInput), 
+            next(nextInput) {}
+};
+
+TriviaNode* t1 = new TriviaNode(
+    "How long was the shortest war on record? (Hint: how many minutes)", 
+    "38", 
+    100
+);
+
+TriviaNode* t2 = new TriviaNode(
+    "What was Bank of America’s original name? (Hint: Bank of Italy or Bank of Germany)", 
+    "Bank of Italy", 
+    50
+);
+
+TriviaNode* t3 = new TriviaNode(
+    "What is the best-selling video game of all time? (Hint: Call of Duty or Wii Sports)",
+    "Wii Sports",
+    20
+);
+
+t1->next = t2;
+t2->next = t3;
+
+
+int main() {
+    cout << "test";
+    return 1;
+}
