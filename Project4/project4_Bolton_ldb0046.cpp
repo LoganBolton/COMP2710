0ff45dcec8b6b312c3c1f244d26091f41a8cdebc
Logan Bolton <loganbolton101@gmail.com>
2023-07-17 13:16:40 -0500

    iterate

--- Project4/project4_Bolton_ldb0046.cpp
+++ Project4/project4_Bolton_ldb0046.cpp
@@ -21,29 +21,35 @@ struct TriviaNode {
             next(nextInput) {}
 };
 
-TriviaNode* t1 = new TriviaNode(
-    "How long was the shortest war on record? (Hint: how many minutes)", 
-    "38", 
-    100
-);
-
-TriviaNode* t2 = new TriviaNode(
-    "What was Bank of America’s original name? (Hint: Bank of Italy or Bank of Germany)", 
-    "Bank of Italy", 
-    50
-);
-
-TriviaNode* t3 = new TriviaNode(
-    "What is the best-selling video game of all time? (Hint: Call of Duty or Wii Sports)",
-    "Wii Sports",
-    20
-);
-
-t1->next = t2;
-t2->next = t3;
-
 
 int main() {
-    cout << "test";
+
+    TriviaNode* t1 = new TriviaNode(
+        "How long was the shortest war on record? (Hint: how many minutes)", 
+        "38", 
+        100
+    );
+
+    TriviaNode* t2 = new TriviaNode(
+        "What was Bank of America’s original name? (Hint: Bank of Italy or Bank of Germany)", 
+        "Bank of Italy", 
+        50
+    );
+
+    TriviaNode* t3 = new TriviaNode(
+        "What is the best-selling video game of all time? (Hint: Call of Duty or Wii Sports)",
+        "Wii Sports",
+        20
+    );
+
+    t1->next = t2;
+    t2->next = t3;
+    
+    TriviaNode* current = t1;
+    while (current != nullptr) {
+        cout << current->question << endl;
+        current = current->next;
+    }
+
     return 1;
 }
