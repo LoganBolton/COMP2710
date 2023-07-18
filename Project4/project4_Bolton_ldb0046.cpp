8c77482758d7f0fab23ecf929209d9d8502eba28
Logan Bolton <loganbolton101@gmail.com>
2023-07-18 13:55:34 -0500

    conditional

--- Project4/project4_Bolton_ldb0046.cpp
+++ Project4/project4_Bolton_ldb0046.cpp
@@ -1,4 +1,4 @@
-// I used GeeksForGeeks to learn how to structure a linked list with C++ syntax
+// I used ChatGPT to learn the syntax of a linked list in C++
 #include <iostream>
 #include <string>
 using namespace std;
@@ -14,9 +14,9 @@ struct TriviaNode {
 
     TriviaNode(
         string questionInput, 
-            string answerInput, 
-            int pointsInput, 
-            TriviaNode* nextInput = nullptr)
+        string answerInput, 
+        int pointsInput, 
+        TriviaNode* nextInput = nullptr)
 
         : question(questionInput), 
             answer(answerInput), 
@@ -49,71 +49,74 @@ int main() {
 
     string newQuestion;
     string newAnswer;
-    int newPoints;
     string newPointsString;
+    int newPoints;
     string userAnswer;
-
-    //hard coded questions
-    TriviaNode* t1 = new TriviaNode(
-        "How long was the shortest war on record? (Hint: how many minutes)", 
-        "38", 
-        100
-    );
-    TriviaNode* t2 = new TriviaNode(
-        "What was Bank of America’s original name? (Hint: Bank of Italy or Bank of Germany)", 
-        "Bank of Italy", 
-        50
-    );
-    TriviaNode* t3 = new TriviaNode(
-        "What is the best-selling video game of all time? (Hint: Call of Duty or Wii Sports)",
-        "Wii Sports",
-        20
-    );
-    t1->next = t2;
-    t2->next = t3;
     
-    cout << "*** Welcome to Log's trivia quiz game ***" << endl;
+    #ifdef prod
+        cout <<"prod";
+        //hard coded questions
+        TriviaNode* t1 = new TriviaNode(
+            "How long was the shortest war on record? (Hint: how many minutes)", 
+            "38", 
+            100
+        );
+        TriviaNode* t2 = new TriviaNode(
+            "What was Bank of America’s original name? (Hint: Bank of Italy or Bank of Germany)", 
+            "Bank of Italy", 
+            50
+        );
+        TriviaNode* t3 = new TriviaNode(
+            "What is the best-selling video game of all time? (Hint: Call of Duty or Wii Sports)",
+            "Wii Sports",
+            20
+        );
+        t1->next = t2;
+        t2->next = t3;
+        
+        cout << "*** Welcome to Log's trivia quiz game ***" << endl;
 
-    // Input Question
-    cout << "Enter a question: ";
-    getline(cin, newQuestion);
-    // cin >> newQuestion;
-    
-    cout << "Enter an answer: ";
-    getline(cin, newAnswer);
-    // cin >> newAnswer;
+        // Input Question
+        cout << "Enter a question: ";
+        getline(cin, newQuestion);
+        
+        cout << "Enter an answer: ";
+        getline(cin, newAnswer);
 
-
-    cout << "Enter award points: ";
-    // getline(cin, newPointsString);
-    cin >> newPointsString; 
-    newPoints = stoi(newPointsString);
-    cin.ignore();
-    addTrivia(t1, newQuestion, newAnswer, newPoints);
-    
-    cout << endl;
-    TriviaNode* current = t1;
-    for (int i = 0; i < totalQuestions; i++) {
-        cout << "Question: " << current->question << endl;
-        cout << "Answer: ";
-        getline(cin, userAnswer);
-
-        if (isCorrect(userAnswer, current)) {
-            cout << "Your answer is correct. ";
-            cout << "You receive " + to_string(current->points) + " points." << endl;
-            totalPoints += current->points;
-            cout << "Your Total Points: " + to_string(totalPoints) << endl;
-        }
-        else {
-            cout << "Your answer is wrong. ";
-            cout << "The correct answer is: " + current->answer << endl;
-            cout << "Your Total Points: " + to_string(totalPoints) << endl;
-
-        }
-
-        current = current->next;
+        cout << "Enter award points: ";
+        cin >> newPointsString; 
+        newPoints = stoi(newPointsString);
+        cin.ignore();
+        addTrivia(t1, newQuestion, newAnswer, newPoints);
+        
         cout << endl;
-    }
+        TriviaNode* current = t1;
+        for (int i = 0; i < totalQuestions; i++) {
+            cout << "Question: " << current->question << endl;
+            cout << "Answer: ";
+            getline(cin, userAnswer);
+
+            if (isCorrect(userAnswer, current)) {
+                cout << "Your answer is correct. ";
+                cout << "You receive " + to_string(current->points) + " points." << endl;
+                totalPoints += current->points;
+                cout << "Your Total Points: " + to_string(totalPoints) << endl;
+            }
+            else {
+                cout << "Your answer is wrong. ";
+                cout << "The correct answer is: " + current->answer << endl;
+                cout << "Your Total Points: " + to_string(totalPoints) << endl;
+
+            }
+
+            current = current->next;
+            cout << endl;
+        }
+    #endif
+
+    #ifdef UNIT_TESTING
+        cout << "testing";
+    #endif
 
     return 0;
 }
