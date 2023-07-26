6b3d5e3cb162580026232a3f8fb59d0b8ac4a88a
Logan Bolton <loganbolton101@gmail.com>
2023-07-26 16:26:23 -0500

    working delete

--- Final/linkedList.cpp
+++ Final/linkedList.cpp
@@ -6,10 +6,10 @@ struct Node {
 	Node* next;
 
 	Node(std::string dataInput, Node* nextInput = NULL) : data(dataInput), next(nextInput) {}
-	
 };
 
 int traverse(Node* n) {
+	std::cout << n->data << std::endl;
 	while (n != NULL) {
 		std::cout << n->data << std::endl;
 		n = n->next;
@@ -18,24 +18,41 @@ int traverse(Node* n) {
 	return 0;
 }
 
-int deleteNode(Node* head, std::string data) {
-	Node* current = head->next;
-	while (current != NULL) {
-		if (current->data == data) {
-			c
-		}
-	}
+void deleteNode(Node*& head, std::string data) {
+    if (head == NULL) {
+        return;
+    }
+
+    if (head->data == data) {
+        Node* oldHead = head;
+        head = head->next;
+        delete oldHead;
+        return;
+    }
+
+    Node* prev = head;
+    Node* current = head->next;
+    while (current != NULL) {
+        if (current->data == data) {
+            prev->next = current->next;
+            delete current;
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
 }
+
 int main() {
 	Node* n4 = new Node("4");
 	Node* n3 = new Node("3", n4);
 	Node* n2 = new Node("2", n3);
 	Node* n1 = new Node("1", n2);
 
-	traverse(n1);
+	deleteNode(n1, "1");
+	traverse(n1); // Traverse the list starting from n1
+
 	return 0;
 }
 
-
-
-
+ 
